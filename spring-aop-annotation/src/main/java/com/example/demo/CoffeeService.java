6aaf1dd7e2e0715c4bf327a9b8c8bee35e8cf560
Eddy.Kim <sieunkr@gmail.com>
2020-04-19 17:23:48 +0900

    ㅇ

--- spring-aop-annotation/src/main/java/com/example/demo/CoffeeService.java
+++ spring-aop-annotation/src/main/java/com/example/demo/CoffeeService.java
@@ -9,12 +9,13 @@ public class CoffeeService {
     public void test_a() {
 
         try {
+            //비즈니스 로직 수행
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-
+    
     @CustomAnnotation
     public void test_b(String name) {
 
